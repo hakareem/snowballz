eab2ef5aaf9ee608a4c522d8ff09b80efb84b4b6
an-brd <94487869+an-brd@users.noreply.github.com>
2021-12-31 13:02:20 +0000

    resize and pin

--- classes/game.js
+++ classes/game.js
@@ -243,5 +243,9 @@ export class Game {
         img.src = fileName;
         return img;
     }
+    resizeCanvas() {
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+    }
 }
 //# sourceMappingURL=game.js.map
--- classes/game.ts
+++ classes/game.ts
@@ -261,4 +261,8 @@ export class Game {
     img.src= fileName
     return img
   }
+  resizeCanvas() {
+    this.canvas.width = window.innerWidth
+    this.canvas.height = window.innerHeight
+  }
 }
