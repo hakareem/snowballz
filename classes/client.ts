2f714318067f9f2f7578b7644c4410780435902a
Mustafa Unal <94565017+MustafaxUK@users.noreply.github.com>
2021-12-31 10:21:49 +0000

    ghostMode almost complete

--- classes/client.ts
+++ classes/client.ts
@@ -2,17 +2,18 @@
 import { Vector } from './vector.js'
 
 
-let gameId:number =0
-let playerName:string=""
-export const endpoint="http://localhost:5050"   //change to the correct deployed port
+let gameId: number = 0
+let playerName: string = ""
+export const endpoint = "http://quicksnooker.com:5050"
+//change to the correct deployed port
 
 // export async function createGame(){
 //     playerName=(<HTMLInputElement>document.getElementById("playerName")).value
 //     let cmd={cmd:"createGame",playerName:playerName}
 //     let gameInfo= await fetchObject(endpoint,cmd)
-    
+
 //     // processMsgs(msgs)
-    
+
 //     gameId=gameInfo.gameId;  //we now know which game WE have joined (the creator)
 //     (<HTMLInputElement>document.getElementById("gameToShare")).value=gameId.toString() 
 //     joinGame(gameId)
@@ -23,7 +24,7 @@ export const endpoint="http://localhost:5050"   //change to the correct deployed
 //  let position: Vector = new Vector(Math.random()* 2000,Math.random()* 2000)
 //  let payload={cmd:"joinGame",playerName:playerName,gameId:gameId,params:{position: position}} //will return (assign to you)a player ID -
 //     let msgs=await fetchObject(endpoint,payload)
- 
+
 //     processMsgs(msgs) //just to display them
 
 //     if (msgs[0].gameId==-1){
@@ -32,7 +33,7 @@ export const endpoint="http://localhost:5050"   //change to the correct deployed
 //     else{        
 //         gameId=msgs[0].gameId        
 //         console.log("Joined game")
-    
+
 //         setInterval(poll,1000) //start polling for incomming data
 //     }        
 
@@ -44,7 +45,7 @@ export const endpoint="http://localhost:5050"   //change to the correct deployed
 //     //all commands must have a cmd, playerName,gameId and *optionally* a params *object* containing arbitrary parameters
 //     let cmd= {cmd:"runTo",playerName:playerName,gameId:gameId,params:{x:100,y:400}} //this will be 'echod' back to us (along with any other pending messages)
 //     let msgs:any[] = await fetchObject(endpoint,cmd) //send our 'command' up - and receive all pending messages
-    
+
 //     processMsgs(msgs) //Output/process the messages
 
 // }
@@ -52,15 +53,15 @@ export const endpoint="http://localhost:5050"   //change to the correct deployed
 
 
 
-export async function fetchObject(url: string,payload:any){
-    const method="POST"
-    const headers={'Accept':'text/html','Content-Type':'application/json'}
-    const response = await fetch(url, {method:method,body:JSON.stringify(payload),headers:headers})
+export async function fetchObject(url: string, payload: any) {
+    const method = "POST"
+    const headers = { 'Accept': 'text/html', 'Content-Type': 'application/json' }
+    const response = await fetch(url, { method: method, body: JSON.stringify(payload), headers: headers })
     //const response = await fetch(url, {method:method,headers:{'Accept':'text/html','Content-Type':'application/json'}})
-    if (response.ok){
-        return await response.json()   
+    if (response.ok) {
+        return await response.json()
     }
-    else{
+    else {
         console.log(`unexpected response status ${response.status} + ${response.statusText}`)
     }
 }
