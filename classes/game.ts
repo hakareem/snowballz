7870f74221d8acd6ac482bb3c0b1179874fb47dc
Harrison frost <harrisonfrost15@gmail.com>
2021-12-31 17:09:20 +0000

    minor changes

--- classes/game.ts
+++ classes/game.ts
@@ -180,7 +180,7 @@ export class Game {
       }
       else {
         // p.runToPoint(p.target);
-        let payload = { cmd: "runToPoint", playerName: this.myName, gameId: this.id, params: { position: p.target } }
+        let payload = { cmd: "runToPoint", playerName: this.myName, gameId: this.id, params: { destination: p.target, position: p.position, health: p.hp } }
         let msgs = await fetchObject(endpoint, payload)
 
         this.processMsgs(msgs) //just to display them
@@ -258,7 +258,9 @@ export class Game {
         }
         else if (m.cmd == "runToPoint") {
           let player = this.players[m.playerName]
-          player.runToPoint(Vector.trueVector(m.params.position))
+          player.position = Vector.trueVector(m.params.position)
+          player.hp = m.params.health
+          player.runToPoint(Vector.trueVector(m.params.destination))
         }
 
         else if (m.cmd == "gameData") {
