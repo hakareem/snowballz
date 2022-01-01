9e079e78588d83e98d6deef5c276b399559b3a20
hakareem <harithakareem72.com>
2022-01-01 22:22:01 +0000

    minor changes

--- classes/snowball.ts
+++ classes/snowball.ts
@@ -50,7 +50,7 @@ export class Snowball {
           if(p.stamina <= 0){
             p.velocity = p.velocity.subtract(p.velocity)
           }
-          Sound.play('playerGasp', 0.5)
+          Sound.play('playerGasp', 0.01)
         }
       }
     }
@@ -61,7 +61,7 @@ export class Snowball {
       let snowballToObstacle = Vector.distanceBetween(obstacle.position, this.position)
       if (snowballToObstacle < obstacle.radius + game.snowballRadius) {
         this.active = false
-        Sound.play('impact', 0.5)
+        Sound.play('impact', 0.01)
       }
     }
   }
