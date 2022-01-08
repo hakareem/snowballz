9f3bee3d2abc872a0702568fbf89e92ee2b89810
Nick <nickax@gmail.com>
2022-01-08 18:04:58 +0000

    Added deathlist

--- classes/player.ts
+++ classes/player.ts
@@ -104,8 +104,10 @@ export class Player {
   }
   move() {
     this.position = this.position.add(this.velocity.multiply(this.stamina / 70 + 0.1) );
+    
     this.stamina -= this.velocity.length / 26  // burn stamina
     this.stamina += 0.15 // regen stamina
+    
     if(this.stamina <= 0 ){
       this.stamina = 0
     }else if(this.stamina > 100){
